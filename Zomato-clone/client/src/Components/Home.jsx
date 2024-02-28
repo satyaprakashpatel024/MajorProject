7f9a64a7be81a443109d535d9de39edd78cf78f6
satyaprakashpatel024 <satyaprakashpatel61@gmail.com>
2024-02-28 14:51:48 +0530

    changes saved

--- Zomato-clone/client/src/Components/Home.jsx
+++ Zomato-clone/client/src/Components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../Home.css';
+import './Home.css';
 import { NavLink } from 'react-router-dom';
 import { FaUserAlt } from 'react-icons/fa';
 import { BiLogInCircle } from 'react-icons/bi';
