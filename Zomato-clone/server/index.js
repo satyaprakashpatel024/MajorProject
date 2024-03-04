105447a4e8b293c596104b51b88aa9db5b8fd016
satyaprakashpatel024 <satyaprakashpatel61@gmail.com>
2024-03-05 00:42:52 +0530

    changes saved

--- Zomato-clone/server/index.js
+++ Zomato-clone/server/index.js
@@ -7,6 +7,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({origin:process.env.CLIENT_URL,credential:true}));
 
+// user routes
 const userRoute = require('./routes/userRoutes.js');
 const restraurantRoute = require('./routes/restraurantRoutes.js');
 const productRoute = require('./routes/productRoutes.js');
