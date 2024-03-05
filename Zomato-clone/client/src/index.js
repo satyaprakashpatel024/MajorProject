6efa60e3a288ef7b63fb6cc09230f4385dd4136d
satyaprakashpatel024 <satyaprakashpatel61@gmail.com>
2024-03-05 23:10:17 +0530

    changes saved

--- Zomato-clone/client/src/index.js
+++ Zomato-clone/client/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom'
-import dotenv from 'dotenv';
-dotenv.config();
+import {BrowserRouter} from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -18,4 +19,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
+reportWebVitals();
